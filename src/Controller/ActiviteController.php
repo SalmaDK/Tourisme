af140abf7782149ba6f68abd7607ce77b98658ec
Your Name <you@example.com>
2023-07-26 21:10:09 +0100

    change the desein

--- src/Controller/ActiviteController.php
+++ src/Controller/ActiviteController.php
@@ -22,20 +22,18 @@ class ActiviteController extends AbstractController
     public function index(EntityManagerInterface $entityManager): Response
     {
 
-        // $query = $entityManager->createQueryBuilder()
-        //     ->select('e.id','a.id as ida','e.nom','e.adresse','e.description','e.image', 'e.linkMap')
-        //     ->from(Endroit::class, 'e')
-        //     ->join(Activite::class, 'a', 'WITH', 'a.idEndroit = e.id')
-        //     ->getQuery();
-        //     $results = $query->getResult();
+        $query = $entityManager->createQueryBuilder()
+            ->select('e.id','a.id as ida','e.nom','e.adresse','e.description','e.image', 'e.linkMap')
+            ->from(Endroit::class, 'e')
+            ->join(Activite::class, 'a', 'WITH', 'a.idEndroit = e.id')
+            ->getQuery();
+            $results = $query->getResult();
 
-        // return $this->render('activite/afficherActivite.html.twig', [
-        //     'activites' => $results,
-        // ]);
-
-        return $this->render('base4.html.twig', [
-            'test' => 'Admin',
+        return $this->render('activite/afficherActivite.html.twig', [
+            'activites' => $results,
         ]);
+
+   
     }
 
 
